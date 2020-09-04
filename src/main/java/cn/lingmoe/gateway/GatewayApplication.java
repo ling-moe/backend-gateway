2acd2dfd97a475a1eacbd718ff2dc4aede051726
yukdawn <yukdawn@gmail.com>
2020-09-04 16:06:01 +0800

    [FIX] 添加spring consul

--- src/main/java/cn/lingmoe/gateway/GatewayApplication.java
+++ src/main/java/cn/lingmoe/gateway/GatewayApplication.java
@@ -3,9 +3,11 @@ package cn.lingmoe.gateway;
 import cn.lingmoe.redis.anno.Enable0moeMultiDbRedis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @Enable0moeMultiDbRedis
+@EnableDiscoveryClient
 public class GatewayApplication {
 
     public static void main(String[] args) {
